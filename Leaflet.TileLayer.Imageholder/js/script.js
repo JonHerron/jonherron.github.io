580e73c0669d41465129be5861d0c74918e3e44a
Ouroboros <herron@hotmail.co.uk>
2021-02-07 06:12:45 +0000

    Add files via upload

---
+++ Leaflet.TileLayer.Imageholder/js/script.js
@@ -0,0 +1,32 @@
+let mapOptions = {
+    zoomControl: true
+};
+
+let mymap = L.map('map', mapOptions).setView([54.91418099296834, -1.3817024230957031], 3);
+
+
+
+let placeholderTile_1_Options = {
+
+}
+
+let placeholderTile_2_Options = {
+    imageholder: {
+        backgroundColour: '3D3D3D', // Hex value, no hash
+        textColour: 'FDFDFD', // Hex value, no hash
+        text: 'WHOOP'
+    }
+};
+
+
+let placeholderTileLayer1 = L.tileLayer.imageholder(placeholderTile_1_Options);
+let placeholderTileLayer2 = L.tileLayer.imageholder(placeholderTile_2_Options);
+
+let baseLayers = {
+    "Imageholder 1": placeholderTileLayer1,
+    "Imageholder 2": placeholderTileLayer2
+};
+
+L.control.layers(baseLayers).addTo(mymap);
+
+placeholderTileLayer1.addTo(mymap);
